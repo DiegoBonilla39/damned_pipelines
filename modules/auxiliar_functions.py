ad0e3ff4e04cf6cc2c1d4b4756bd3913ccf03a7a
DiegoBonilla39 <diegobonilla39@gmail.com>
2023-04-01 14:22:57 +0200

    damned_pipelines

--- modules/auxiliar_functions.py
+++ modules/auxiliar_functions.py
@@ -1,4 +1,5 @@
 #imports
+
 import requests
 import pandas as pd
 import re
@@ -6,16 +7,16 @@ import json
 import math
 import os
 
-#auxiliar_functions
+#functions
 
 # Aux Function 1: You can get only 100 results per page so it is important to know the number of pages you'll need.
 
 def pages(base_url, search, state, username, api_token):
     pages = requests.get(base_url + search.format(state), auth=(username,api_token)).json()['total_count']
-    if state == 'open':
+    if STATE == 'open':
         pages = math.ceil(pages/100)
         return pages
-    elif state == 'closed':
+    elif STATE == 'closed':
         pages = math.ceil(pages/100)
         return pages
     
@@ -78,46 +79,3 @@ def time_parser(x):
         return x
     except:
         return 'Nothing pushed yet'
-
-#pipeline_functions
-
-# Pipeline Function 1: And finally get the 'pull requests'.
-
-def get_pulls(base_url, key, owner, repo, pulls, search, state, username, api_token, field_list):
-    pulls_list = []
-    max_pages = pages(base_url, search, state, username, api_token)
-    for i in range(max_pages):
-        r_pulls = requests.get(base_url + key + owner + repo + pulls.format(i+1, state),
-                               auth=(username, api_token)).json()
-        df_pulls = pd.json_normalize(r_pulls)
-        pulls_list.append(df_pulls)
-    df_pulls = pd.concat(pulls_list)
-    df_pulls = df_pulls[field_list]
-    return df_pulls
-
-# Pipeline Function 2: Apply!!!!!!
-
-def df_status(df_pulls, base_url, key, owner, repo, commits, username, api_token, field_list):
-    df_pulls['student_name'] = df_pulls['title'].apply(student_name)
-    df_pulls['lab_name'] = df_pulls['title'].apply(lab_name)
-    df_pulls['created_at'] = df_pulls['created_at'].apply(time_parser)
-    df_pulls['updated_at'] = df_pulls['updated_at'].apply(time_parser)
-    df_pulls['head.repo.pushed_at'] = df_pulls['head.repo.pushed_at'].apply(time_parser)
-    df_pulls['lab_status'] = df_pulls.apply(lambda col: get_commits(base_url,
-                                                                    key,
-                                                                    owner,
-                                                                    repo,
-                                                                    commits,
-                                                                    col['number'],
-                                                                    username,
-                                                                    api_token), axis=1)
-    df_status = df_pulls[field_list]
-    return df_status
-
-# Pipeline function 3: And there you have it!!!
-
-def create_csv(df_status, field_sort, field_name):
-    df_csv = df_status.sort_values(by=field_sort, ascending=False)
-    df_csv.columns = field_name
-    df_csv.to_csv('./data/labs_status.csv', index=False)
-    return df_csv
